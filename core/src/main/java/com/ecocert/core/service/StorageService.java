c1b74be4834e447221d8847f4ca4afb8829d88a7
Lucian <l.palaghe@gmail.com>
2019-06-08 16:07:47 +0300

    Added image uniqueness logic

--- core/src/main/java/com/ecocert/core/service/StorageService.java
+++ core/src/main/java/com/ecocert/core/service/StorageService.java
@@ -2,8 +2,10 @@ package com.ecocert.core.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface StorageService {
     void createDirectory();
-    public String store(MultipartFile file);
+    public String store(MultipartFile file) throws IOException;
 
 }
--- core/src/main/java/com/ecocert/core/service/StorageServiceImpl.java
+++ core/src/main/java/com/ecocert/core/service/StorageServiceImpl.java
@@ -5,6 +5,7 @@ import com.ecocert.core.model.StorageException;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -13,55 +14,62 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.UUID;
 
 @Service
 public class StorageServiceImpl implements StorageService {
 
-    private final Path rootLocation;
+	private final Path rootLocation;
 //TODO create check for image  type accepted
 
-    @Autowired
-    public StorageServiceImpl(StorageDirectory storageDirectory) {
-        this.rootLocation = Paths.get(storageDirectory.getLocation());
-    }
+	@Autowired
+	public StorageServiceImpl(StorageDirectory storageDirectory) {
+		this.rootLocation = Paths.get(storageDirectory.getLocation());
+	}
 
-    @Override
-    public void createDirectory() {
-        try {
-            Files.createDirectories(rootLocation);
-        } catch (IOException e) {
-            throw new StorageException("Could not initialize storage", e);
-        }
-    }
+	@Override
+	public void createDirectory() {
+		try {
+			Files.createDirectories(rootLocation);
+		} catch (IOException e) {
+			throw new StorageException("Could not initialize storage", e);
+		}
+	}
 
-    @Override
-    public String store(MultipartFile file) {
-        UUID uid= UUID.randomUUID();
-        String fileName =uid + "."+ FilenameUtils.getExtension(file.getOriginalFilename());
-        try {
-            if (file.isEmpty()) {
-                throw new StorageException("Failed to store empty file " + fileName);
-            }
-            if (fileName.contains("..")) {
-                // This is a security check
-                throw new StorageException(
-                        "Cannot store file with relative path outside current directory "
-                                + fileName);
-            }
-            try (InputStream inputStream = file.getInputStream()) {
-                Files.copy(inputStream, this.rootLocation.resolve(fileName),
-                        StandardCopyOption.REPLACE_EXISTING);
-            }
-        } catch (IOException e) {
-            throw new StorageException("Failed to store file " + fileName, e);
-        }
-        return fileName;
-    }
+	@Override
+	public String store(MultipartFile file) throws IOException {
+		String fileName = null;
+		try {
+			if (file.isEmpty()) {
+				throw new StorageException("Failed to store empty file " + file.getOriginalFilename());
+			}
+			if (file.getOriginalFilename().contains("..")) {
+				// This is a security check
+				throw new StorageException(
+						"Cannot store file with relative path outside current directory "
+								+ file.getOriginalFilename());
+			}
+			String md5Hex = DigestUtils.md5DigestAsHex(file.getBytes()).toUpperCase();
+			fileName = md5Hex + "." + FilenameUtils.getExtension(file.getOriginalFilename());
+			String finalFileName = fileName;
+			boolean fileExists = Files.list(this.rootLocation).anyMatch(e -> e.endsWith(finalFileName));
+			if (fileExists) {
+				return finalFileName;
+			}
 
-    public void getCoordinates(MultipartFile image) throws IOException {
-        javaxt.io.Image img = new javaxt.io.Image(image.getInputStream());
-        double[] gps = img.getGPSCoordinate();
-        System.out.println(gps);
-    }
+
+			try (InputStream inputStream = file.getInputStream()) {
+				Files.copy(inputStream, this.rootLocation.resolve(fileName),
+						StandardCopyOption.REPLACE_EXISTING);
+			}
+		} catch (IOException e) {
+			throw new StorageException("Failed to store file " + file.getOriginalFilename(), e);
+		}
+		return fileName;
+	}
+
+	public void getCoordinates(MultipartFile image) throws IOException {
+		javaxt.io.Image img = new javaxt.io.Image(image.getInputStream());
+		double[] gps = img.getGPSCoordinate();
+		System.out.println(gps);
+	}
 }
