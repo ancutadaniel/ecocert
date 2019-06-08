de565c99bba387a88e4ecc7d71b8e43f811504d7
fnitu <fnitu@users.noreply.github.com>
2019-06-08 12:02:10 +0300

    - add upload form

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
-      <input id="upload-photo" accept="image/*" hidden="" type="file" />
+
+      <form action="http://192.168.34.8:8081/images" method="post" encType="multipart/form-data">
+        <input name="image" accept="image/*" type="file" />
+        <input type="submit" value="Upload" name="submit" />
+      </form>
     </div>
   );
 }
