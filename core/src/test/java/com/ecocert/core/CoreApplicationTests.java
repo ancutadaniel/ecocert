6f8a064dc977c6e6349d27e2ef52314007033e16
Lucian <l.palaghe@gmail.com>
2019-06-08 10:02:59 +0300

    Core api skeleton
    Core api skeleton

---
+++ core/src/test/java/com/ecocert/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecocert.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
