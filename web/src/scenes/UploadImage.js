db5f02211c2d37fe0b9a6bb80f82a2cdcf5446c8
fnitu <fnitu@users.noreply.github.com>
2019-07-27 10:36:42 +0300

    add upload image

---
+++ web/src/scenes/UploadImage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from "react-bootstrap/Container"; 
+class UploadImage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <Container fluid className="contact_container">
+                <p>Test Upload</p>
+            </Container>
+        );
+    }
+}
+
+
+export default UploadImage;
